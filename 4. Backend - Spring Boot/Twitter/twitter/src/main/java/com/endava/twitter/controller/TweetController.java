f5254b93833a20495b5f2016ab2915a586792677
Sofia <asztangie2@gmail.com>
2022-03-10 00:37:51 -0500

    Squash
    Squashed a couple commits 
    
    This reverts commit 3c8531312887624e3bb63d78af3f83dc8eeea8d9, reversing
    changes made to 2d6f0052b4211257b4e41e7338e4c32ed467d43f.

---
+++ 4. Backend - Spring Boot/Twitter/twitter/src/main/java/com/endava/twitter/controller/TweetController.java
@@ -0,0 +1,206 @@
+package com.endava.twitter.controller;
+
+import com.endava.twitter.model.entity.PublicUser;
+import com.endava.twitter.model.entity.TweetSorting;
+import com.endava.twitter.model.mapper.PublicUserMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.endava.twitter.model.dto.TweetDto;
+import com.endava.twitter.service.TweetService;
+import com.endava.twitter.response.TweetResponseTemplate;
+import com.endava.twitter.model.dto.UserDto;
+import com.endava.twitter.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//import javax.validation.Valid;
+import org.springframework.validation.annotation.Validated;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+public class TweetController {
+
+    @Autowired
+    private TweetService tweetService;
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/tweet/{tweetId}")
+    ResponseEntity<TweetDto> searchById(@RequestHeader String userId, @PathVariable String tweetId){
+        userService.findById(userId);
+        TweetDto tweetDto = tweetService.findById(tweetId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header("userId", userId)
+                .body(tweetDto);
+    }
+
+    @GetMapping("/tweets/All")
+    ResponseEntity<List<TweetDto>> findAll(){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(tweetService.findAll());
+    }
+
+    @GetMapping("/tweets/sorting")
+    ResponseEntity<List<TweetDto>> findAllSorting(@RequestParam String sortBy){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(tweetService.findAllSorting(sortBy));
+    }
+
+    @GetMapping("tweets/page")
+    ResponseEntity <Page<TweetDto>> findAllOnPages( @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "limit", required = false) Integer size){
+
+        //Default Values
+        page = page != null ? page : 0;
+        size = size != null? size : 100;
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body( tweetService.findAllOnPages(page, size));
+    }
+
+    @GetMapping("/timeline")
+    ResponseEntity<Page<TweetDto>> loadTweets(){
+        return findFiltering(null, null, null, null, null);
+    }
+
+    @GetMapping("/tweets")
+    ResponseEntity<Page<TweetDto>> findFiltering(
+            @RequestParam(value = "page", required = false) Integer page,
+            @RequestParam(value = "limit", required = false) Integer size,
+            @RequestParam(value = "sort", required = false) String sortBy,
+            @RequestParam(value = "user", required = false) List<String> filtersUser,
+            @RequestParam(value = "search", required = false) List<String> filtersText
+    ){
+
+        //Default Values
+        page = page != null ? page : 0;
+        size = size != null? size : 100;
+        sortBy = sortBy != null? sortBy : TweetSorting.NEWEST.toString();
+        filtersUser = filtersUser != null? filtersUser : new ArrayList<>(Collections.singletonList(""));
+        filtersText = filtersText != null ? filtersText : new ArrayList<>(Collections.singletonList(""));
+
+        //Format : Filtering
+        List<UserDto> userDtoFilters = userService.findFiltersById(filtersUser);
+        List<PublicUser> publicUserFilters = userDtoFilters
+                .stream()
+                .map(PublicUserMapper.PUBLIC_USER_INSTANCE::toEntity)
+                .distinct()
+                .collect(Collectors.toList());
+
+        List<String> textFullFilters = filtersText.stream()
+                .map(s -> tweetService.findAllTextsContaining(s))
+                .flatMap(Collection::stream)
+                .map(TweetDto::getText)
+                .distinct()
+                .collect(Collectors.toList());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(tweetService.findFiltering(page, size, sortBy, publicUserFilters, textFullFilters));
+    }
+
+    @PostMapping("/tweets")
+    ResponseEntity<TweetResponseTemplate> addNewTweet(@RequestHeader String userId, @RequestBody @Validated TweetDto tweet) {
+        UserDto userDto = userService.findById(userId);
+        TweetDto tweetDto = tweetService.addNewTweet(tweet, userDto);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("userId", userId)
+                .header("tweetId", tweetDto.getId())
+                .body(
+                        new TweetResponseTemplate(
+                                "Your new tweet has been created successfully.",
+                                tweetDto
+                        )
+                );
+
+    }
+
+    @PutMapping("/tweets")
+    ResponseEntity<TweetResponseTemplate> updateTweet(@RequestHeader String userId, @RequestParam String tweetId, @RequestBody @Validated TweetDto tweet){
+        userService.findById(userId);
+        TweetDto tweetDto = tweetService.updateTweet(userId, tweetId, tweet);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header("userId", userId)
+                .header("tweetId", tweetId)
+                .body(
+                        new TweetResponseTemplate(
+                            "Tweet has been updated successfully.",
+                            tweetDto
+                        )
+                );
+    }
+
+    @DeleteMapping("/tweets/{tweetId}")
+    ResponseEntity<String> deleteTweet(@RequestHeader String userId, @PathVariable String tweetId){
+        userService.findById(userId);
+        tweetService.deleteTweet(userId, tweetId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header("userId", userId)
+                .header("tweetId", tweetId)
+                .body("Tweet has been deleted successfully.");
+
+    }
+
+    @PostMapping("/favorites/create")
+    ResponseEntity<TweetResponseTemplate> addToFavorites(@RequestHeader String userId, @RequestParam String tweetId){
+        userService.findById(userId);
+        TweetDto tweetDto = tweetService.addToFavorites(userId, tweetId);
+        userService.addToFavorites(userId, tweetId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header("userId", userId)
+                .header("tweetId", tweetId)
+                .body(
+                        new TweetResponseTemplate(
+                                "Tweet has been added to favourites successfully.",
+                                tweetDto
+                        )
+                );
+    }
+
+    @PostMapping("/favorites/destroy")
+    ResponseEntity<TweetResponseTemplate> removeFromFavorites(@RequestHeader String userId, @RequestParam String tweetId){
+        userService.findById(userId);
+        TweetDto tweetDto = tweetService.removeFromFavorites(userId, tweetId);
+        userService.removeFromFavorites(userId, tweetId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header("userId", userId)
+                .header("tweetId", tweetId)
+                .body(
+                        new TweetResponseTemplate(
+                                "Tweet has been removed from favourites successfully.",
+                                tweetDto
+                        )
+                );
+    }
+
+
+
+}
--- README.md
+++ README.md
@@ -18,9 +18,22 @@ Evaluate how you solve an optimization problem using dynamic programming.
 - Validate your capacity of determine the time complexity of a dynamic algorithm
 - Validate your capacity of determine the space complexity of a dynamic algorithm
   
-> - [<font color="violet">Java Project</font>](https://github.com/Safimmi/EndavaChallenges/tree/master/2.%20Dynamic%20Programming/vacuum) :memo:
+> - [<font color="violet">Java Project - Rework</font>](https://github.com/Safimmi/EndavaChallenges/tree/master/2.%20Dynamic%20Programming/vacuum) :memo:
 > - [Problem description document](https://github.com/Safimmi/EndavaChallenges/blob/master/2.%20Dynamic%20Programming/challenge%20dp.pdf)
 > - [Review Slides](https://github.com/Safimmi/EndavaChallenges/blob/master/2.%20Dynamic%20Programming/Challenge%20-%20Dynamic%20Programming.pptx)
+
+
+## 4. Back End - Spring Boot
+#### *Week 1* 
+
+As an Endava customer, we want to have an application like twitter to be able to use it internally in our company.
+To start this project, we want to have basic functionalities, and depending on the needs, we will add more requirements in the future. As a first step,
+we want an API that exposes some endpoints so in the future the front-end team can use them.
+  
+> - [<font color="violet">Spring Boot Project</font>](https://github.com/Safimmi/EndavaChallenges/tree/master/4.%20Backend%20-%20Spring%20Boot/Twitter/twitter) :memo:
+> - [<font color="violet">Swagger Path</font>](http://localhost:8885/swagger-ui.html) :memo:
+> - [Problem description document](https://github.com/Safimmi/EndavaChallenges/blob/master/4.%20Backend%20-%20Spring%20Boot/Spring%20Boot%20Twitter%20Challenge/Spring%20Boot%20Twitter%20Challenge.pdf)
+
 
 
 
